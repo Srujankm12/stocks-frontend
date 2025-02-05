a92e3b83b9e7246b60c1f7e01de135a687eccbf1
Srujankm12 <srujankm12@gmail.com>
2025-02-05 16:10:34 +0530

    no issues

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -9,9 +9,10 @@
 </script>
 
 <header class="bg-black text-white fixed w-full z-50 shadow-md">
-    <!-- Navbar container -->
+
     <div class="container  flex items-center px-6 py-3">
-        <!-- Hamburger Menu on the left -->
+
+        <!-- svelte-ignore a11y_consider_explicit_label -->
         <button
             class="text-3xl bg-black text-white shadow-md focus:outline-none"
             on:click={toggleDrawer}
@@ -19,18 +20,20 @@
             <i class="fa-solid fa-bars"></i>
         </button>
 
-        <!-- Logo and Branding, centered in the remaining space -->
+      
         <div class="flex items-center justify-center flex-grow">
             <img src="/logo.jpeg" alt="SRA BAO Logo" class="w-12 h-12 rounded-full mr-4" />
             <h1 class="text-2xl font-light">SR Automation</h1>
         </div>
 
-        <!-- Optional Right-side Element (if needed) -->
+
         <div class="flex items-center justify-end"></div>
     </div>
 
-    <!-- Sidebar Drawer -->
+
     {#if isDrawerOpen}
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <!-- svelte-ignore a11y_click_events_have_key_events -->
         <div class="fixed inset-0 bg-black bg-opacity-50 z-40" on:click={toggleDrawer}></div>
         <div class="fixed top-0 left-0 h-full w-64 bg-white shadow-lg rounded-lg z-50 transform transition-transform duration-300 ease-in-out">
             <div class="p-6 flex flex-col h-full">
